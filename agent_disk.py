76395ee224da2b02c45a1168fc09950c7bf84666
Nikhil Desai <nikhildesai1997@gmail.com>
2020-10-26 15:17:19 -0400

    changes

--- agent_disk.py
+++ agent_disk.py
@@ -26,3 +26,42 @@ y_circles = Y*np.ones(num*2+1) + s1*w/2*math.sin(math.radians(yaw))    # the y-c
 disc = list(zip(x_circles,y_circles))
 disc_collection.append(disc)
 
+import gym
+import highway_env
+env = gym.make('highway-v0')
+env.config
+config = {
+"observation": {
+    "offscreen_rendering": True,
+    "type": "Kinematics",
+    'action': {'type': 'DiscreteMetaAction'},
+     'simulation_frequency': 15,
+     'policy_frequency': 1,
+     'other_vehicles_type': 'highway_env.vehicle.behavior.IDMVehicle',
+     'screen_width': 600,
+     'screen_height': 150,
+     'centering_position': [0.3, 0.5],
+     'scaling': 5.5,
+     'show_trajectories': False,
+     'render_agent': True,
+     "features": ["presence", "x", "y", "vx", "vy","cos_h","sin_h"],  # changed added
+     'manual_control': False,
+     'lanes_count': 4,
+     'vehicles_count': 50,
+     'duration': 40,
+     'initial_spacing': 2,
+     'collision_reward': -1,
+     'offroad_terminal': False,
+     'normalize': False,   # changed added
+     "absolute": True      # changed added
+                }
+
+        }
+
+env.configure(config)
+env.reset()
+for i in range(100):
+    done = False
+    while not done:
+        observation_, reward, done, info = env.step(env.action_space.sample())
+        env.render()
--- ego_vehicle_ray_disk_copy.py
+++ ego_vehicle_ray_disk_copy.py
@@ -3,6 +3,14 @@
 """
 
       *******************      Deep Reinforcement learning  **********************
+      
+      vehicles = 15
+      feature = presence,x,y,vx,vy,cosh,sinh
+      gamma = 0.8
+      batch_size=32
+      max_mem_size=15000
+      target update = 50
+      
 
 """
 import torch as T
@@ -16,7 +24,7 @@ import scipy
 import math
 
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter(f'runs/try')
+#writer = SummaryWriter(f'runs/try')
 
 
 class DeepQNetwork(nn.Module):
@@ -33,6 +41,7 @@ class DeepQNetwork(nn.Module):
 
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
+        
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
@@ -95,6 +104,21 @@ class Agent():
         self.terminal_memory[index] = terminal
         #print(self.mem_cntr)
         self.mem_cntr += 1
+        
+    def choose_action_testing(self,observation,lidar):
+        observation = observation.flatten()         
+        observation = np.float32(observation)
+        lidar = np.float32(lidar)
+        
+        state = T.tensor([observation]).to(self.Q_eval.device)
+        lidar = T.tensor([lidar]).to(self.Q_eval.device)
+        
+        state_lidar = T.cat([state,lidar],dim = 1)
+        
+        actions = self.Q_eval.forward(state_lidar)
+        action = T.argmax(actions).item()
+        
+        return action
 
     def choose_action(self, observation,lidar):
         if np.random.random() > self.epsilon:
@@ -142,15 +166,21 @@ class Agent():
      
         q_target = reward_batch + self.gamma*T.max(q_next,dim=1)[0]
 
-        loss = F.mse_loss(q_target,q_eval)
-        writer.add_scalar('training loss',loss,global_step=i)
+
+        loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
+        #writer.add_scalar('training loss',loss,global_step=i)
         loss.backward()
         self.Q_eval.optimizer.step()
+        #print(loss)
+        
+        
 
         self.iter_cntr += 1
         
-        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min \
-                       else self.eps_min
+        #self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min \
+                       #else self.eps_min
+                       
+        self.epsilon = self.eps_min + (1.0 - self.eps_min) * np.exp(-self.eps_dec * i)
                        
         if self.iter_cntr % self.replace_target == 0:
             self.Q_next.load_state_dict(self.Q_eval.state_dict())
@@ -294,34 +324,37 @@ if __name__ == '__main__':
     
     config = {
     "observation": {
-        "offscreen_rendering": True,
-        "type": "Kinematics",
-        "vehicles_count": 15,
-        "initial_spacing":10,
-        "features": ["presence", "x", "y", "vx", "vy","cos_h","sin_h"],
-        "features_range": {
-            "x": [0, 100],
-            "y": [0, 100],
-            "vx": [-20, 20],
-            "vy": [-20, 20]
-        },
-        "lanes_count": 2,
-        "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
-        "absolute": True,
-        "order": "sorted",
-        'render_agent': True,
-        "normalize":False,
-        "duration":100,
-        'simulation_frequency': 20,
+    "offscreen_rendering": True,
+    "type": "Kinematics",
+    'action': {'type': 'DiscreteMetaAction'},
+     'simulation_frequency': 15,
+     'policy_frequency': 1,
+     'other_vehicles_type': 'highway_env.vehicle.behavior.IDMVehicle',
+     'screen_width': 600,
+     'screen_height': 150,
+     'centering_position': [0.3, 0.5],
+     'scaling': 5.5,
+     'show_trajectories': False,
+     'render_agent': True,
+     "features": ["presence", "x", "y", "vx", "vy","cos_h","sin_h"],  # changed added
+     'manual_control': False,
+     'lanes_count': 4,
+     'vehicles_count': 15,
+     'duration': 40,
+     'initial_spacing': 2,
+     'collision_reward': -1,
+     'offroad_terminal': False,
+     'normalize': False,   # changed added
+     "absolute": True      # changed added
+                }
 
-                    }
-            }
+        }
 
     #env = gym.make('highway-v0')
     env.configure(config)                       # Update our configuration in the environment
     env.reset()
     env = gym.wrappers.Monitor(env, "./vid", video_callable=lambda episode_id: True,force=True)
-    agent = Agent(max_mem_size=50000,gamma=0.99, epsilon=1.0, lr=0.003,input_dims=[105+540], batch_size=32, n_actions=5,lidar_input_dims=[540],eps_end=0.01)
+    agent = Agent(max_mem_size=15000,gamma=0.8, epsilon=1.0, lr=0.003,input_dims=[105+540], batch_size=32, n_actions=5,lidar_input_dims=[540],eps_end=0.05)
     n_games = 4000
     scores,eps_history,avg_score,speed_at_collision = [],[],[],[]
     env = gym.wrappers.Monitor(env, "./vid", video_callable=lambda episode_id: True,force=True)
@@ -339,7 +372,6 @@ if __name__ == '__main__':
                 
                 current_lidar = lidar.flatten()
                 lidar = np.concatenate((current_lidar,lidar_frame1,lidar_frame2))   # current, (prev frame), (prev prev frame)
-            
                 action = agent.choose_action(observation,lidar)
                 
                 observation_, reward, done, info = env.step(action)
@@ -370,13 +402,13 @@ if __name__ == '__main__':
                     'epsilon %.2f' % agent.epsilon)
             
             # Adding to tensorboard
-            
+            """
             writer.add_scalar('reward',score,global_step=i)
             writer.add_scalar('epsilon',agent.epsilon,global_step=i)
             writer.add_scalar('avg_score',avg_score,global_step=i)
             writer.add_scalar('speed_at_collision',speed_at_collision,global_step=i)
             writer.add_scalar('avg_speed_epsiode',avg_speed,global_step=i)
-            
+            """
             # Adding to local file
             out_string = ""
             out_string+=str(i)
@@ -389,6 +421,62 @@ if __name__ == '__main__':
             out_string+="\n"
             out_file.write(out_string)
         
+    # Testing    
+    
+    with open('Test.csv','w') as test_file:
+        for i in range(500):
+            lidar_frame1 = np.zeros(180)
+            lidar_frame2 = np.zeros(180)
+            score = 0
+            speed_in_episode = []
+            crashed = False
+            observation = env.reset()
+            done = False
+            while not done:
+                lidar = display_lidar(observation)
+                
+                current_lidar = lidar.flatten()
+                lidar = np.concatenate((current_lidar,lidar_frame1,lidar_frame2))   # current, (prev frame), (prev prev frame)
+                action = agent.choose_action_testing(observation,lidar)
+                
+                observation_, reward, done, info = env.step(action)
+                env.render()
+                score+=reward
+                
+                lidar_frame2 = lidar_frame1
+                lidar_frame1 = current_lidar
+                
+                speed_in_episode.append(math.sqrt(math.pow(observation[0][3],2)+math.pow(observation[0][4], 2)))
+                
+                if(info['crashed'] == True):
+                    crashed = True
+                
+                
+            scores.append(score)
+            speed_at_collision = math.sqrt(math.pow(observation[0][3],2)+math.pow(observation[0][4], 2))
+            eps_history.append(agent.epsilon)
+            avg_score = np.mean(scores[-100:])
+            avg_speed = np.mean(speed_in_episode)
+            print('episode ', i, 'score %.2f' % score,
+                    'average score %.2f' % avg_score)
+            
+            # Adding to tensorboard
+            """
+            writer.add_scalar('reward_testing',score,global_step=i)
+            writer.add_scalar('avg_score_testing',avg_score,global_step=i)
+            writer.add_scalar('speed_at_collision_testing',speed_at_collision,global_step=i)
+            writer.add_scalar('avg_speed_epsiode_testing',avg_speed,global_step=i)
+            """
+            # Adding to local file
+            test_string = ""
+            test_string+=str(i)
+            test_string+=","+str(score)
+            test_string+=","+str(avg_score)
+            test_string+=","+str(speed_at_collision)
+            test_string+=","+str(avg_speed)
+            test_string+=","+str(crashed)
+            test_string+="\n"
+            test_file.write(test_string)
 
 
 
